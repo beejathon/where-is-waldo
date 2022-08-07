ca9af16d3b72cee3969c99eb0c7003cd43933f92
Bee-jay <bjpaiz@gmail.com>
2022-08-07 17:55:31 +0100

    First commit, renders clickable image that records coordinates of mouse clicks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import waldo from './assets/waldo.jpg';
+import './App.css'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAM1ToAPFdJqzdaxuk_nrqmALmYlzRsur4",
+  authDomain: "wru-waldo.firebaseapp.com",
+  projectId: "wru-waldo",
+  storageBucket: "wru-waldo.appspot.com",
+  messagingSenderId: "384388119225",
+  appId: "1:384388119225:web:42364104139afd6bff3968"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
+  const [click, setClick] = useState({x: 0, y: 0})
+  const [show, setShow] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const x = e.clientX - e.target.offsetLeft;
+    const y = e.clientY - e.target.offsetTop;
+    
+    setClick({x: x, y: y});
+    setShow(true);
+  }
+
+  useEffect(() => {
+    console.log(click);
+  }, [click])
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img 
+          src={waldo} 
+          alt=""
+          onMouseDown={(e) => handleClick(e)}
+          className="waldo-img"
+        />
+      </div>
     </div>
   );
 }
