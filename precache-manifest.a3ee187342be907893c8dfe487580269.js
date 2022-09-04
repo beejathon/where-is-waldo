1ca9dda2d92829cc0b2c94e37b1496eaf0ee0c97
Bee-jay <bjpaiz@gmail.com>
2022-09-04 13:29:19 +0100

    Updates

---
+++ precache-manifest.a3ee187342be907893c8dfe487580269.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8f5a31b23b119e06065",
+    "url": "/where-is-waldo/static/css/main.7a194e99.chunk.css"
+  },
+  {
+    "revision": "f8f5a31b23b119e06065",
+    "url": "/where-is-waldo/static/js/main.f8f5a31b.chunk.js"
+  },
+  {
+    "revision": "8e869f294817ed29c951",
+    "url": "/where-is-waldo/static/css/1.6ca31e80.chunk.css"
+  },
+  {
+    "revision": "8e869f294817ed29c951",
+    "url": "/where-is-waldo/static/js/1.8e869f29.chunk.js"
+  },
+  {
+    "revision": "fb9632a84a180ce26dea",
+    "url": "/where-is-waldo/static/js/runtime~main.fb9632a8.js"
+  },
+  {
+    "revision": "7cb0ae022863ac2606b88b473e3f3565",
+    "url": "/where-is-waldo/static/media/1.7cb0ae02.png"
+  },
+  {
+    "revision": "9375420d37ef552efba12273f9e2914f",
+    "url": "/where-is-waldo/static/media/2.9375420d.png"
+  },
+  {
+    "revision": "fd6f1646ddfd3bedb14800b92f59a573",
+    "url": "/where-is-waldo/static/media/3.fd6f1646.png"
+  },
+  {
+    "revision": "46d7908b4bae7039e3c036bd9b580dc4",
+    "url": "/where-is-waldo/static/media/4.46d7908b.png"
+  },
+  {
+    "revision": "1b187642c2bb4f1996ea5cb856fb3eaf",
+    "url": "/where-is-waldo/static/media/5.1b187642.png"
+  },
+  {
+    "revision": "e7d972c5f70b75a4be1898643205f8b5",
+    "url": "/where-is-waldo/static/media/waldo.e7d972c5.jpg"
+  },
+  {
+    "revision": "af550bf21578e5d67b12048b4b5e651a",
+    "url": "/where-is-waldo/index.html"
+  }
+];
