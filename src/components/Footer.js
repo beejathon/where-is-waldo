f192c2ce59c7ecd57148ac987a44f58c9fce41f1
Bee-jay <bjpaiz@gmail.com>
2022-09-05 20:41:24 +0100

    Add README

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       </div>
       <div className="mdl-mini-footer__right-section">
         <img className="git-logo" src={github} alt="github logo" />
-        Created by<a href="https://github.com/beejathon"> Bee-jay Paiz</a> as part of <a href="https://www.theodinproject.com/">The Odin Project</a> Javascript curriculum
+        Created by <a href="https://github.com/beejathon">Bee-jay Paiz</a> as part of <a href="https://www.theodinproject.com/">The Odin Project</a> Javascript curriculum
       </div>
     </footer>
   )
